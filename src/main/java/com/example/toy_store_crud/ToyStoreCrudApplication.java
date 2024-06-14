fcfdf53364674d17cbcda15ec98b270a99876761
Leo-Martins-Dev <leonardomartins578@gmail.com>
2024-06-14 16:11:59 -0300

    :tada: First Commit

---
+++ src/main/java/com/example/toy_store_crud/ToyStoreCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_store_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyStoreCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyStoreCrudApplication.class, args);
+	}
+
+}
