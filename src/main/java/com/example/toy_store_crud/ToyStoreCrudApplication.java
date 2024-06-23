e878eef90a0e2d7203f63f55433751a2491e3317
lmartns <leonardomartins578@gmail.com>
2024-06-23 12:35:48 -0300

    🚀 feat: update ToyStoreCrudApplication and add new teste controller

--- src/main/java/com/example/toy_store_crud/ToyStoreCrudApplication.java
+++ src/main/java/com/example/toy_store_crud/ToyStoreCrudApplication.java
@@ -9,5 +9,4 @@ public class ToyStoreCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToyStoreCrudApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/toy_store_crud/controllers/teste.java
@@ -0,0 +1,4 @@
+package com.example.toy_store_crud.controllers;
+
+public class teste {
+}
