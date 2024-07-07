459d06eea95a747a45056b8133a0c99cf170169e
lmartns <leonardomartins578@gmail.com>
2024-07-06 21:31:30 -0300

    📦 feat: add new product domain model (Toy.java) and update application properties

---
+++ src/main/java/com/example/toy_store_crud/domain/product/Toy.java
@@ -0,0 +1,20 @@
+package com.example.toy_store_crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "toy")
+@Entity(name = "Toy")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Toy {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Float price;
+}
