3941990d74b5d8178418528db03434dc5a6839e0
lmartns <leonardomartins578@gmail.com>
2024-07-14 20:39:31 -0300

    💻 feat: update README, POM, and Java files for toy store CRUD, Add Doc for Endpoints

--- src/main/java/com/example/toy_store_crud/controllers/ToyController.java
+++ src/main/java/com/example/toy_store_crud/controllers/ToyController.java
@@ -5,7 +5,10 @@ import com.example.toy_store_crud.domain.dto.DeleteToyDto;
 import com.example.toy_store_crud.domain.dto.UpdateToyDto;
 import com.example.toy_store_crud.domain.entity.Toy;
 import com.example.toy_store_crud.repository.ToyRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,10 @@ public class ToyController {
     @Autowired
     private ToyRepository toyRepository;
 
+    @Operation(summary = "Get a toy by id")
+    @ApiResponse(responseCode = "200", description = "Found the toy")
+    @ApiResponse(responseCode = "404", description = "Toy not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/{id}")
     public ResponseEntity getToy(@PathVariable String id) {
         var toy = toyRepository.findById(id);
@@ -25,19 +32,31 @@ public class ToyController {
         return ResponseEntity.ok(toy.get());
     }
 
+    @Operation(summary = "Get all toys")
+    @ApiResponse(responseCode = "200", description = "Found all toys")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "404", description = "Toys not found")
     @GetMapping
     public ResponseEntity getAllToys() {
         var allToys = toyRepository.findAll();
         return ResponseEntity.ok(allToys);
     }
 
+    @Operation(summary = "Create a toy")
+    @ApiResponse(responseCode = "201", description = "Toy created")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    @ApiResponse(responseCode = "400", description = "Bad request")
     @PostMapping
     public ResponseEntity createToy(@RequestBody @Validated CreateToyDto createToyDto) {
         Toy newToy = new Toy(createToyDto);
         toyRepository.save(newToy);
-        return ResponseEntity.ok(newToy);
+        return new ResponseEntity<>(newToy, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a toy")
+    @ApiResponse(responseCode = "200", description = "Toy updated")
+    @ApiResponse(responseCode = "404", description = "Toy not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PutMapping("/{id}")
     public ResponseEntity updateToy(@PathVariable String id, @RequestBody UpdateToyDto updateToyDto) {
         var toy = toyRepository.findById(id);
--- src/main/java/com/example/toy_store_crud/domain/entity/Toy.java
+++ src/main/java/com/example/toy_store_crud/domain/entity/Toy.java
@@ -1,6 +1,7 @@
 package com.example.toy_store_crud.domain.entity;
 
 import com.example.toy_store_crud.domain.dto.CreateToyDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,12 +12,16 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Schema(name = "Toy", description = "Toy entity")
 public class Toy {
     @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Schema(description = "Toy id", example = "d1fb2726-4efe-48e2-bcda-902d0500629e", required = true)
     private String id;
 
+    @Schema(description = "Toy name", example = "Buzz", required = true)
     private String name;
 
+    @Schema(description = "Toy price", example = "1000", required = true)
     private Float price;
 
     public Toy(CreateToyDto createToyDto) {
